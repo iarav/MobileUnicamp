b50c3b5a3f971d0f61efc9cddebd1fce110a57e0
Isabella <isabella-arj@hotmail.com>
2023-06-28 01:29:22 -0300

    criando logica de verificar data

--- projeto_mobile/lib/view/PagesUsuario/selecaoDataOpcoesReserva.dart
+++ projeto_mobile/lib/view/PagesUsuario/selecaoDataOpcoesReserva.dart
@@ -1,7 +1,15 @@
 // ignore_for_file: library_private_types_in_public_api
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
+import '../../bloc/bloc_state.dart';
+import '../../bloc/dadosReservas/dadosReservas_bloc.dart';
+import '../../bloc/dadosReservas/dadosReservas_event.dart';
+import '../../bloc/dataBloqueada/dataBloqueada_bloc.dart';
+import '../../bloc/dataBloqueada/dataBloqueada_event.dart';
 import '../../model/routes.dart';
 
 class SelecaoData extends StatefulWidget {
@@ -13,8 +21,8 @@ class SelecaoData extends StatefulWidget {
 
 class _SelecaoDataState extends State<SelecaoData> {
   DateTime? _selectedDate;
-
   final TextEditingController _dateController = TextEditingController();
+  late bool estaDisponivel;
 
   @override
   Widget build(BuildContext context) {
@@ -46,95 +54,17 @@ class _SelecaoDataState extends State<SelecaoData> {
               height: 20,
             ),
             ElevatedButton(
-              onPressed: () => {
+              onPressed: () async => {
                 if (_selectedDate != null)
                   {
-                    showDialog<String>(
-                      context: context,
-                      builder: (BuildContext context) => Dialog(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              const SizedBox(height: 30),
-                              Text(
-                                'A data: ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year} está disponível para reserva!',
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 30),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  TextButton(
-                                    onPressed: () {
-                                      _selectedDate = null;
-                                      _dateController.text = '';
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('Cancelar'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pushNamed(
-                                        context,
-                                        Routes
-                                            .fazerReserva, //define your route name
-                                        arguments:
-                                            _selectedDate, //pass the arguments
-                                      );
-                                    },
-                                    child: const Text('Fazer Reserva'),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                    dialogDisponibilidade(true),
                   }
                 else
                   {
-                    showDialog<String>(
-                      context: context,
-                      builder: (BuildContext context) => Dialog(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              const SizedBox(height: 30),
-                              const Text(
-                                'Selecione uma data!',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 30),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                    print("saaaaa"),
+                    estaDisponivel = await verificarDisponibilidade(),
+                    print("Esta disponivel: ${estaDisponivel}"),
+                    // dialogDisponibilidade(false),
                   }
               },
               style: ElevatedButton.styleFrom(
@@ -201,5 +131,140 @@ class _SelecaoDataState extends State<SelecaoData> {
         _selectDate(context);
       },
     );
+  }
+
+  Future<String?> dialogDisponibilidade(disponivel) {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => Dialog(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 30),
+              (disponivel == true)
+                  ? //se disponivel faça
+                  Text(
+                      'A data: ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year} está disponível para reserva!',
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  : //senão faça
+                  const Text(
+                      'Selecione uma data!',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: (disponivel == true)
+                    ? //se disponivel faça
+                    [
+                        TextButton(
+                          onPressed: () {
+                            _selectedDate = null;
+                            _dateController.text = '';
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Cancelar'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                              context,
+                              Routes.fazerReserva, //define your route name
+                              arguments: _selectedDate, //pass the arguments
+                            );
+                          },
+                          child: const Text('Fazer Reserva'),
+                        )
+                      ]
+                    : // senão faça
+
+                    [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  
+  Completer<bool> meuCompleter = Completer<bool>();
+
+  Future<bool> verificarDisponibilidade() async {
+    final blocReservas = context.read<DadosReservasBloc>();
+    final blocReservasBloqueadas = context.read<DataBloqueadaBloc>();
+    List<Map<String, dynamic>> datasNaoDisponiveis = [];
+
+    late bool disponivel;
+
+    StreamSubscription? reservasSubscription;
+    StreamSubscription? bloqueadasSubscription;
+
+    //ADICIONA EM DATASNAODISPONIVEIS DE RESERVAS
+    blocReservas.add(GetAllDadosReservasEvent());
+    reservasSubscription = blocReservas.stream.listen((state1) async {
+      if (state1 is LoadedState) {
+        if (state1.dados != null) {
+          for (var value in state1.dados!.values) {
+            if (value is Map<String, dynamic>) {
+              datasNaoDisponiveis.add({
+                'datas': value['dataReserva'],
+              });
+            }
+          }
+
+          //ADICIONA EM DATASNAODISPONIVEIS DE DATAS BLOQUEADAS
+          blocReservasBloqueadas.add(GetAllDataBloqueadaEvent());
+          bloqueadasSubscription = blocReservasBloqueadas.stream.listen((state2) async {
+            if (state2 is LoadedState) {
+              if (state2.dados != null) {
+                for (var value in state2.dados!.values) {
+                  if (value is Map<String, dynamic>) {
+                    datasNaoDisponiveis.add({
+                      'datas': value['data'],
+                    });
+                  }
+                }
+              }
+            }
+            if (reservasSubscription != null && bloqueadasSubscription != null) {
+              print("DATAS INDISPONÍVEIS: ");
+              print(datasNaoDisponiveis);
+              String dataParaVerificar = "30/06/2023";
+
+              //VERIFICA SE A DATA ESTÁ OU NÃO DISPONÍVEL
+              bool dataEstaDisponivel = datasNaoDisponiveis.any((map) {
+                String data = map['datas'] as String;
+                return data == dataParaVerificar;
+              });
+              disponivel = !dataEstaDisponivel;
+
+              if (!meuCompleter.isCompleted) {
+                meuCompleter.complete(disponivel);
+              }
+            }
+          });
+        }
+      }
+    });
+
+    await meuCompleter.future;
+    return disponivel;
   }
 }
